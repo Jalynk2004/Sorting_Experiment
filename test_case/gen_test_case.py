6e22ac3d3c24c4b8cd54a23d63fd7f49c10811bc
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2023-03-11 11:13:32 -0500

    Add files via upload

---
+++ test_case/gen_test_case.py
@@ -0,0 +1,18 @@
+import random 
+def gen_test():
+	a = []
+	for i in range(1000000):
+		a.append(round(random.uniform(10, 100000), 4))
+	return a
+def write(filename, arr):
+    	f = open(filename, "w")
+    	print(*arr, file = f, sep = " ")
+    	f.close()
+for i in range(0, 10):
+    	a = gen_test()
+    	if i == 0:
+        	a.sort()
+    	elif i == 1:
+        	a.sort()
+        	a.reverse()
+    	write("testcase{}.txt".format(i + 1), a)
