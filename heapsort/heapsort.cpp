45803675fa00d6118784cdb3c3a83b1c799739d7
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2023-03-12 11:58:49 -0400

    Update heapsort.cpp

--- heapsort/heapsort.cpp
+++ heapsort/heapsort.cpp
@@ -2,7 +2,6 @@
   using namespace std;
   
   void heapify(float arr[], int n, int i) {
-    // Find largest among root, left child and right child
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
@@ -13,24 +12,19 @@
     if (right < n && arr[right] > arr[largest])
       largest = right;
   
-    // Swap and continue heapifying if root is not largest
     if (largest != i) {
       swap(arr[i], arr[largest]);
       heapify(arr, n, largest);
     }
   }
   
-  // main function to do heap sort
-  void heapSort(float arr[], int n) {
-    // Build max heap
-    for (int i = n / 2 - 1; i >= 0; i--)
+    void heapSort(float arr[], int n) {
+        for (int i = n / 2 - 1; i >= 0; i--)
       heapify(arr, n, i);
   
-    // Heap sort
-    for (int i = n - 1; i >= 0; i--) {
+      for (int i = n - 1; i >= 0; i--) {
       swap(arr[0], arr[i]);
   
-      // Heapify root element to get highest element at root again
       heapify(arr, i, 0);
     }
 }
