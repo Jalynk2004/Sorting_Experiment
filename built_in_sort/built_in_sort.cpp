f4f8d49bd7877fca303942e8e2adc7a0c4ec0f07
Jalynk2004 <81233886+Jalynk2004@users.noreply.github.com>
2023-03-12 11:39:12 -0400

    Add files via upload

---
+++ built_in_sort/built_in_sort.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+char file_name[200];
+int main(){
+	auto *a = new float[1000001]; 
+	for (unsigned int i = 0; i < 10; i++) {
+		memset(file_name, 0, sizeof(file_name));
+		sprintf(file_name, "testcase%u.txt", i + 1);
+		std::ifstream File(file_name);
+		for (unsigned int i = 0; i < 1000000 && (!File.eof()); i++){
+			File >> a[i];
+		}
+		File.close();	
+		auto begin = std::chrono::high_resolution_clock::now(); 
+		std::sort(a, a + 1000000);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration =  std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+		std::cout << "Time to C++ built-in sort for " << file_name << " is " << duration.count() << " microseconds\n"; 
+	}
+	free(a)
+	return 0;
+}
